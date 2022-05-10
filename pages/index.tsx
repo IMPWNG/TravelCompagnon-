1e730f1b2cf826ca8e18886a75391f0e91c2a3aa
IMPWNG <matisse.ach+github.com>
2022-05-10 12:09:09 -0400

    Folder Structure + Header Start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+
+import Header from './components/Header/Header';
 
 
 const Home: NextPage = () => {
   return (
-<>
-    <div className="text-white border-4">
-      test
-    </div>
-</>
+    <>
+
+      <Header />
+
+    </>
   )
 }
 
