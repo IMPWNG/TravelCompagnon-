2077cd973e49a5aaa4cdc3f2cdcbd7f7affe944f
IMPWNG <matisse.ach+github.com>
2022-05-10 12:02:00 -0400

    To NextJs + Tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+<>
+    <div className="text-white border-4">
+      test
+    </div>
+</>
+  )
+}
+
+export default Home
--- src/components/PlaceDetails/PlaceDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PlaceDetails() {
-  return (
-    <div>PlaceDetails</div>
-  )
-}
