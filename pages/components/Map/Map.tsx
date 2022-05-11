1de30940f012b94458e49e6a9df9e4bbbf5ec666
IMPWNG <matisse.ach+github.com>
2022-05-11 10:09:58 -0400

    apiKey srting issue

--- pages/components/Map/Map.tsx
+++ pages/components/Map/Map.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
 
-export default function Map({ coords }: { coords: { lat: number; lng: number } }) {
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+export default function Map() {
+
+    const apiKey: string | undefined = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+    const containerStyle = {
+        width: '400px',
+        height: '400px'
+    };
+
+    const center = {
+        lat: -3.745,
+        lng: -38.523
+    };
 
     return (
-        <>
         <div className='w-full h-4/5'>
-            <GoogleMapReact
-                bootstrapURLkeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-                defaultCenter={coords}
-                center={coords}
-                defaultZoom={14}
-                margin={[50, 50, 50, 50]}
+            <LoadScript
+                googleMapsApiKey={`${apiKey}`}
             >
-            </GoogleMapReact>
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={10}
+                >
+                    { /* Child components, such as markers, info windows, etc. */}
+                    <></>
+                </GoogleMap>
+            </LoadScript>
+    
         </div>
-        <div>
-            dede
-        </div>
-        </>
+      
     )
 }
